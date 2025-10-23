8c51f1160c258d987cfa0c3969cf361d1a6aa094
Bablu162731 <kuppireddyavinashreddy2@gmail.com>
2025-10-23 14:07:12 +0530

    Add files via upload

---
+++ intro.sql
@@ -0,0 +1,37 @@
+show databases;
+-- create database avinash;
+-- use avinash;
+-- create table school_names
+-- (
+-- 	school_id INT,
+--     school_place varchar(50),
+--     school_name varchar(50),
+--     school_branches int);
+
+-- insert into school_names(school_id, school_place, school_name, school_branches)
+-- values(1, 'pdtr', 'st.joseph', 3),
+-- 	  (2, 'hyd', 'vaagdevi', 4),
+--       (3, 'kadapa', 'ksrm', 2);
+-- select * from school_names;
+
+create database bvs;
+use bvs;
+create table localwala
+(
+	emp_id int,
+    emp_name varchar(50),
+    emp_age int,
+    emp_established varchar(50)
+    );
+    select * from localwala;
+    insert into localwala(emp_id, emp_name, emp_age, emp_established)
+    values(1, 'srinath', 22, 'proddatur'),
+    (2,'venky', 22,'kadapa'),
+    (3,'bablu', 22, 'pulivendula');
+    
+    
+    
+    
+
+
+ 
